2f915628d3a17951e05d406e8161d1e019cb29fe
steelbrain <aneesiqbalbhatti@gmail.com>
2015-03-23 07:26:45 -0700

    :new: Add EventEmitter Class

---
+++ Source/EventEmitter.hh
@@ -0,0 +1,36 @@
+<?hh //strict
+class EventEmitter{
+  public Map<string, Vector<(function(string):void)>> $Callbacks = Map{};
+  public function __construct(){
+
+  }
+  public function on(string $Event, (function(string):void) $Callback):this{
+    if(!$this->Callbacks->contains($Event)){
+      $this->Callbacks->set($Event, Vector{});
+    }
+    $this->Callbacks->get($Event)?->add($Callback);
+    return $this;
+  }
+  public function off(string $Event, ?(function(string):void) $Callback):this{
+    if($this->Callbacks->contains($Event)){
+      if($Callback === null){
+        $this->Callbacks->remove($Event);
+      } else {
+        $Index = $this->Callbacks->get($Event)?->linearSearch($Callback);
+        if($Index !== null && $Index !== -1){
+          $this->Callbacks->get($Event)?->removeKey($Index);
+        }
+      }
+    }
+    return $this;
+  }
+  public function emit(string $Event, string $Args):this{
+    $Vector = $this->Callbacks->contains($Event) ? $this->Callbacks->get($Event) : null;
+    if($Vector !== null){
+      foreach($Vector as $Callback){
+        $Callback($Args);
+      }
+    }
+    return $this;
+  }
+}
