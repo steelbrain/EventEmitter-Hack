318cc25da4e16b44ee33374591bdc5076b7d5025
steelbrain <aneesiqbalbhatti@gmail.com>
2015-03-23 07:54:10 -0700

    :new: Implemented EventEmitter::once

--- Source/EventEmitter.hh
+++ Source/EventEmitter.hh
@@ -1,4 +1,4 @@
-<?hh //strict
+<?hh
 class EventEmitter{
   public Map<string, Vector<(function(string):void)>> $Callbacks = Map{};
   public function on(string $Event, (function(string):void) $Callback):this{
@@ -6,6 +6,18 @@ class EventEmitter{
       $this->Callbacks->set($Event, Vector{});
     }
     $this->Callbacks->get($Event)?->add($Callback);
+    return $this;
+  }
+  public function once(string $Event, (function(string):void) $Callback):this{
+    if(!$this->Callbacks->contains($Event)){
+      $this->Callbacks->set($Event, Vector{});
+    }
+    $WrapperCB = null;
+    $WrapperCB = function(string $Info) use($Callback, $Event, &$WrapperCB) {
+      $Callback($Info);
+      $this->off($Event, $WrapperCB);
+    };
+    $this->Callbacks->get($Event)?->add($WrapperCB);
     return $this;
   }
   public function off(string $Event, ?(function(string):void) $Callback):this{
